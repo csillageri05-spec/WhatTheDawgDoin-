a4e0c1b611f53ad891c69aa5562541c9a0426ecf
Fóti Dávid Bálint (SZF_N_2025_2027) <foti.david.balint@diak.szamalk-szalezi.hu>
2026-09-23 15:46:18 +0200

    konfliktus

--- src/app/KonfliktusKezeles.java
+++ src/app/KonfliktusKezeles.java
@@ -3,6 +3,12 @@ package app;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
+        keveres();
+        
+    }
+
+    private static void keveres() {
+        System.out.println("KONFLIKTUS!!!");
     }
 
 }
